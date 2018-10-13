a0a4f3f7020175476b503ca144f4da246f59b26c
Pc <akhilnp.is.17@nitj.ac.in>
2018-10-13 12:33:13 +0530

    Introduction to Python Added

---
+++ Python-Introduction/Exercise/Exercise2.py
@@ -0,0 +1,9 @@
+def main():
+    food='Burger'
+    if food == 'Pizza':
+        print('Ummmm, my favorite!')
+    else:
+        print("No, I won't have it. I want Pizza!")
+
+if __name__ == '__main__':
+    main()
